911e08ddf0eb05689bb41519b0cb7e9a062c415b
Geoff Coffey <gwcoffey@me.com>
2023-07-29 12:37:24 -0700

    cormacbot: add logging, move schedule ahead a few minutes in case it helps with timeouts, add wip posts from the road

--- netlify/functions/cormacbot/index.ts
+++ netlify/functions/cormacbot/index.ts
@@ -4,6 +4,7 @@ import { env } from "process";
 import post_data from "./posts.json";
 
 const cormacbotHandler: Handler = async (event: HandlerEvent) => {
+    console.log("sending post…");
     await post(post_data, new URL('https://botsin.space'), env['LITBOT_TOKEN_CORMAC']!);
     console.log("Post sent successfully");
     return {
@@ -12,6 +13,6 @@ const cormacbotHandler: Handler = async (event: HandlerEvent) => {
 };
 
 // schedule every saturday morning at 9
-const handler = schedule("0 16 * * 6", cormacbotHandler)
+const handler = schedule("12 16 * * 6", cormacbotHandler)
 
 export { handler };
